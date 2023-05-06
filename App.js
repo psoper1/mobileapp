8c89f8de29e45a5915acff9e00da34a2230b0299
psoper1 <psoper0604@gmail.com>
2023-05-06 11:23:18 -0400

    playing around

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
+
+  const num1 = 5;
+  const num2 = 5;
+
+  const handlePress = () => {
+    Alert.alert("It's " + (num1 + num2))
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Reeeeeee</Text>
+      <Text>Press button to find out what 5 + 5 is</Text>
+      <Button
+      title="Press Me"
+      color={'#FFD433'}
+      onPress={handlePress}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +27,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#3E5568',
     alignItems: 'center',
     justifyContent: 'center',
   },
