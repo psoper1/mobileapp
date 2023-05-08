3c8deac183ae9cce492afe9ad3819d57e8b9333a
psoper1 <psoper0604@gmail.com>
2023-05-07 20:06:05 -0400

    made a toggle between background colors

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import {React, useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
 
-  const num1 = 5;
-  const num2 = 5;
+  const [theStyle, setTheStyle] = useState('#3E5568')
+  const [pressed, setPressed] = useState(false)
 
+  
   const handlePress = () => {
-    Alert.alert("It's " + (num1 + num2))
+    setTheStyle((styles.container.backgroundColor === '#3E5568') ? '#393646' : '#3E5568');
+    setPressed(!pressed)
   }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: theStyle,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
 
   return (
     <View style={styles.container}>
-      <Text>Press button to find out what 5 + 5 is</Text>
       <Button
-      title="Press Me"
+      title={!pressed ? "Press Me to change me back" : "Press Me to change the background"}
       color={'#FFD433'}
       onPress={handlePress}
       />
@@ -24,11 +34,13 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#3E5568',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+{/* <View style={theStyle}> */}
+      {/* <Text>Press button to find out what 5 + 5 is</Text> */}
+      {/* <Button
+      title="Press Me to change the background"
+      color={'#FFD433'}
+      onPress={handlePress}
+      />
+      <StatusBar style="auto" />
+    </View> */}
