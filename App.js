1e52f29084fddc65cc8f329f9a844ccc7271b91d
psoper1 <psoper0604@gmail.com>
2023-05-10 09:42:35 -0400

    commit

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { React, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './Home';
 
 export default function App() {
 
   const [theStyle, setTheStyle] = useState('#3E5568')
   const [pressed, setPressed] = useState(false)
 
+  function Home() {
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Button
+            title={!pressed ? "Press Me to change me back" : "Press Me to change the background"}
+            color={'#FFD433'}
+            onPress={handlePress}
+        />
+    </View>
+    );
+  }
 
   const handlePress = () => {
     setTheStyle((styles.container.backgroundColor === '#3E5568') ? '#393646' : '#3E5568');
@@ -22,14 +37,19 @@ export default function App() {
     },
   });
 
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Button
-        title={!pressed ? "Press Me to change me back" : "Press Me to change the background"}
-        color={'#FFD433'}
-        onPress={handlePress}
-      />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+        <Stack.Screen 
+                  name="Home" 
+                  component={Home}
+                  options={{
+                    headerTitleAlign: 'center'
+                  }}
+                  />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
