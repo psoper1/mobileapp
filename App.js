2f727948b1ce1aa8464a7ce1392ceba8ed2adb21
psoper1 <psoper0604@gmail.com>
2023-05-10 09:24:21 -0400

    made new branch

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import {React, useState} from 'react';
+import { React, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
@@ -7,12 +7,12 @@ export default function App() {
   const [theStyle, setTheStyle] = useState('#3E5568')
   const [pressed, setPressed] = useState(false)
 
-  
+
   const handlePress = () => {
     setTheStyle((styles.container.backgroundColor === '#3E5568') ? '#393646' : '#3E5568');
     setPressed(!pressed)
   }
-  
+
   const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -24,23 +24,12 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Button
-      title={!pressed ? "Press Me to change me back" : "Press Me to change the background"}
-      color={'#FFD433'}
-      onPress={handlePress}
-      />
       <StatusBar style="auto" />
+      <Button
+        title={!pressed ? "Press Me to change me back" : "Press Me to change the background"}
+        color={'#FFD433'}
+        onPress={handlePress}
+      />
     </View>
   );
 }
-
-
-{/* <View style={theStyle}> */}
-      {/* <Text>Press button to find out what 5 + 5 is</Text> */}
-      {/* <Button
-      title="Press Me to change the background"
-      color={'#FFD433'}
-      onPress={handlePress}
-      />
-      <StatusBar style="auto" />
-    </View> */}
